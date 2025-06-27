24e3985d77fd94f91cbcb5ae76ff840d3ec2d5d7
simone-sampino <s.samp98@gmail.com>
2025-06-27 15:34:04 +0200

    Enhance greeting message with chalk and update package.json dependencies

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-console.log("Hello Boolean");
+console.log(chalk.green.bgYellow("Hello Boolean"));
 
+// Bonus
 import "dotenv/config";
+
+import chalk from "chalk";
+
+import axios from "axios";
+
+axios
+  .get("https://lanciweb.github.io/demo/api/pictures/")
+  .then((res) => console.log(res.data));
